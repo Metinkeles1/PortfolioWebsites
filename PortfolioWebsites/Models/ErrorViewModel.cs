58b1dc24b2dbb1843aba4533941846abe827abf9
Metin Keleş <metinkeles677@gmail.com>
2023-04-29 13:56:06 +0300

    Add project files.

---
+++ PortfolioWebsites/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PortfolioWebsites.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
